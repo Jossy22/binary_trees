1992ff4781ebf2b24aee3c9a1847ccaceb480266
Jossy22 <josephineokaforadi@gmail.com>
2024-01-05 13:05:57 -0800

    binary tree

---
+++ tests/113-main.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "../binary_trees.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success, error code on failure
+ */
+int main(void)
+{
+    bst_t *tree;
+    int array[] = {
+        79, 47, 68, 87, 84, 91, 21, 32, 34, 2,
+        20, 22, 98, 1, 62, 95
+    };
+    size_t n = sizeof(array) / sizeof(array[0]);
+    bst_t *node;
+
